a4d7cbedd00fecdb39117f64e37747b5c5976bab
PaoloTroni <paolotroni@gmail.com>
2025-02-23 19:46:58 +0100

    cambio nombre y nombre en variable

--- juego-suma.py
+++ juego-suma.py
@@ -1,5 +1,7 @@
 import time
 
+nombre_jugador = "jugador"
+
 
 def probar_suma_dos_numeros(num1, num2):
 
@@ -7,19 +9,19 @@ def probar_suma_dos_numeros(num1, num2):
     user_res = int(input(f"\n{num1} + {num2} ES ?: "))
     print()
     if res_suma == user_res:
-        print("MUY BIEN, REBECCA 🤪🎊🎉")
+        print(f"MUY BIEN, {nombre_jugador} 🤪🎊🎉")
         print(f"{num1} + {num2}  ES {res_suma}.\n")
     else:
-        print("MAL, REBECCA 🧐😩😭")
+        print(f"MAL, {nombre_jugador} 🧐😩😭")
         print(f"{num1} + {num2}  ES {res_suma}.\n")
 
 
-print("\nHOLA REBECCA \n")
+print(f"\nHOLA {nombre_jugador} \n")
 while True:
 
-    num1 = int(input("\nNUMERO, REBECCA: "))
+    num1 = int(input(f"\nNUMERO, {nombre_jugador}: "))
 
-    num2 = int(input("\nOTRO NUMERO, REBECCA: "))
+    num2 = int(input(f"\nOTRO NUMERO, {nombre_jugador}: "))
     probar_suma_dos_numeros(num1, num2)
 
     print("----------------------------------------------------------------")
